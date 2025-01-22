1c2e98d71137dc9f1f11006fa97dc356eed622b3
DaniilIvanenkov <132118406+DaniilIvanenkov@users.noreply.github.com>
2025-01-22 04:11:50 +0000

    0.98/5

---
+++ src/components/ClickOnePers/ClickOnePers.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import Button from "../Button/Button"
+import { IHogwarts } from "../../entity/Hogwarts.rdo"
+import "./ClickOnePers.css"
+import { useNavigate } from "react-router"
+const click= ()=>{
+    const [person, setPerson] = useState<IHogwarts>();
+    const navigate = useNavigate();
+    return(
+        <div className="full">
+            <img/>
+            <h2>{person?.actor}</h2>
+            <h2></h2>
+            <h2></h2>
+            <h2></h2>
+            <h2></h2>
+            <Button className="cls" onClick={()=> navigate(-1)}>Вернуться назад</Button>
+        </div>
+    )
+}
--- src/components/Onepers/Onepers.tsx
+++ src/components/Onepers/Onepers.tsx
@@ -1,19 +1,22 @@
 import { Navigate, useNavigate } from "react-router"
 import Button from "../Button/Button";
 import { IHogwarts } from "../../entity/Hogwarts.rdo";
-
+import "./Onepers.css"
 
 const Pers = ({name, image, actor, species, gender, house, id}: IHogwarts)=>{
     const navigate = useNavigate();
     return(
-        <div>
+        <div className="divpers">
 
         <img className="imag" src={image}/>
+        <div className="textitems">
+
         <p className="text">Персонаж: {name}</p>
         <p className="text">Актер: {actor}</p>
         <p className="text">Вид: {species}</p>
         <p className="text">Пол: {gender}</p>
         <p className="text">Факультет: {house}</p>
+        </div>
         <Button onClick={()=> navigate("/"+id)}>Посмотреть полную информацию</Button>
         
         </div>
