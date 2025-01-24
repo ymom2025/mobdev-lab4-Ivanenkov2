598c557299de7d16513de08f5887ee07f663e46b
DaniilIvanenkov <132118406+DaniilIvanenkov@users.noreply.github.com>
2025-01-24 05:57:43 +0000

    1.0

--- src/page/PageMain.tsx
+++ src/page/PageMain.tsx
@@ -1,24 +1,43 @@
 import './PageMain.css'
 import Feed from '../components/Feed/Feed'
-import { useState } from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 import { IHogwarts } from '../entity/Hogwarts.rdo'
+import Button from '../components/Button/Button'
 import hogwartsAPI from '../shared/api'
 import Input from '../components/Input/Input'
 
-const PageMain = () =>{
+const PageMain = () => {
     const [value, setValue] = useState("")
     const [perz, setPerz] = useState<IHogwarts[]>([]);
+    const [filterperz, setFilterperz] = useState<IHogwarts[]>([]);
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        setPerz((await hogwartsAPI.axioshogwarts()));
+        //setPerz((await hogwartsAPI.axioshogwarts()));
     }
-    return(
-        <div className="inputandbtn">  
-        <form onSubmit={handleSubmit}>
-            <Input value={value}  setValue={setValue} placevalue="Напишите имя и фамилию персонажа"/>
-            <button>Найти</button>
-            </form>  
-           <Feed perz={perz}></Feed>  
+    
+    useEffect (() =>{
+        setFilterperz(perz.filter((name) => name.name.includes(value)))
+        console.log(filterperz);
+        
+    }, [value]);
+    
+    
+    useEffect(() => {
+        const viZOV = async () => {
+            const response = await hogwartsAPI.axioshogwarts()
+            setFilterperz(response)
+            setPerz(response);
+        }
+        viZOV();
+    }, []);
+    return (
+        <div className="inputandbtn">
+            <form onSubmit={handleSubmit}>
+                <Input value={value} setValue={setValue} placevalue="Напишите имя и фамилию персонажа" />
+                <Button>Найти</Button>
+            </form>
+            <Feed perz={filterperz}></Feed>
         </div>
     )
 }
